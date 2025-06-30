dad0fcb98e5143d8f96894bc1c6ee0c3b388dc92
MatsuiYuto <m1m02530155@oit.ac.jp>
2025-06-30 20:03:17 +0900

    add some methods

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -4,11 +4,25 @@
 package org.example;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+  public String getGreeting() {
+    return "Hello World!";
+  }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
+  public static void main(String[] args) {
+    int a = 2;
+    int b = 3;
+    int c = 1;
+    int d = 10;
+
+    int sum1 = new Calculate().Sum(a, b);
+    int sum2 = new Calculate().SumBetween(c, d);
+
+    System.out.println(new App().getGreeting());
+    System.out.print("Sum of " + a + " and " + b + " is " + sum1 + ". ");
+    System.out.println("Average is " + new Calculate().Average(sum1, 2) + ".");
+    System.out.print("Sum of " + c + " to " + d + " is " + sum2 + ". ");
+    System.out.println("Average is " + new Calculate().Average(sum2, d - c + 1) + ".");
+    System.out.println("Sum of odd of " + c + " to " + d + " is " + new Calculate().OddSumBetween(c, d) + ".");
+    System.out.println("Sum of even of " + c + " to " + d + " is " + new Calculate().EvenSumBetween(c, d) + ".");
+  }
 }
--- app/src/main/java/org/example/Calculate.java
+++ app/src/main/java/org/example/Calculate.java
@@ -1,7 +1,61 @@
 package org.example;
 
 public class Calculate {
-  public int sum(int x, int y) {
+  public int Sum(int x, int y) {
     return x + y;
+  }
+
+  public double Average(int sum, int num) {
+    if (num != 0) {
+      return (double) sum / num;
+    } else {
+      return 0.0;
+    }
+  }
+
+  public int SumBetween(int start, int end) {
+    if (start > end) {
+      int tmp = start;
+      start = end;
+      end = tmp;
+    }
+
+    int sum = 0;
+    for (int i = start; i <= end; i++) {
+      sum += i;
+    }
+    return sum;
+  }
+
+  public int OddSumBetween(int start, int end) {
+    if (start > end) {
+      int tmp = start;
+      start = end;
+      end = tmp;
+    }
+
+    int sum = 0;
+    for (int i = start; i <= end; i++) {
+      if (i % 2 == 1) {
+        sum += i;
+      }
+    }
+    return sum;
+  }
+
+  public int EvenSumBetween(int start, int end) {
+    if (start > end) {
+      int tmp = start;
+      start = end;
+      end = tmp;
+    }
+
+    int sum = 0;
+    for (int i = start; i <= end; i++) {
+      if (i % 2 == 0) {
+        sum += i;
+      }
+    }
+    return sum;
   }
 }
